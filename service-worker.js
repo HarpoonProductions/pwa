6548fee71c28261a4023dc9bdc6986e82ccc758d
HarpoonProductions <110096521+HarpoonProductions@users.noreply.github.com>
2024-06-03 21:47:19 +0100

    pwa

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pwa-v1.4";
+const CACHE_NAME = "pwa-v1.5";
 const urlsToCache = [
   "/pwa/article/article.html",
   "/pwa/article/assets/9ZnpCEPpfk/harpoon-magazine-logo-1563x500.png",
