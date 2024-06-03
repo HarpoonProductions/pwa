d559bea16c408bd47dceb780da54146db863114e
HarpoonProductions <110096521+HarpoonProductions@users.noreply.github.com>
2024-06-03 21:15:18 +0100

    update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "site-static-v4";
+const CACHE_NAME = "site-static-v5";
 const urlsToCache = [
   "/pwa/article/article.html",
   "/pwa/article/assets/9ZnpCEPpfk/harpoon-magazine-logo-1563x500.png",
